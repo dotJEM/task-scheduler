81c28d0e373b60760dbb450a49afc9b01ce86c3a
Jens Melgaard <admin@it-links.dk>
2017-07-04 13:22:41 +0200

    temp desc

--- DotJEM.Scheduler/DotJEM.Scheduler/Properties/AssemblyInfo.cs
+++ DotJEM.Scheduler/DotJEM.Scheduler/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DotJEM.Scheduler")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extracted scheduler from webhost")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("N/A")]
 [assembly: AssemblyProduct("DotJEM.Scheduler")]
